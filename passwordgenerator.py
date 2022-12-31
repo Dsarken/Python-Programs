9ea76e06ba629182a070ad2a9d3c7f5dbb353334
Cameron Guinn <70319422+Dsarken@users.noreply.github.com>
2022-12-31 13:07:00 +0200

    Add files via upload
    A simple password generator with different options

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -1,26 +1,54 @@
-import string
 import random
 
 
-characters = string.ascii_letters + \
-    string.digits + '!@#$%^&*()'
-
-
-def random_password(length):
-    return ''.join(random.choice(characters) for i in range(length))
+def password_generator(choice):
+    random_password = ''
+    new_set = """@#$%^&*1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ{}[]()/\\'"`~,;:.<>"""
+    if choice == 1:
+        for i in range(0, 4):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    if choice == 2:
+        for i in range(0, 8):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    if choice == 3:
+        for i in range(0, 16):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    if choice == 4:
+        for i in range(0, 32):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    if choice == 5:
+        for i in range(0, 48):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    if choice == 6:
+        for i in range(0, 64):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    if choice == 7:
+        for i in range(0, 128):
+            chosen_letter = random.choice(new_set)
+            random_password += chosen_letter
+    print(f"This is your generated password: {random_password}")
+    print(len(random_password))
 
 
 def main():
-    choice = input("Do you want to generate a password?(Yes/No) ")
-    if choice == 'Yes':
-        password_length = int(input("Enter length of password to generate: "))
-        print(random_password(password_length))
-    elif choice == 'No':
-        quit()
-    else:
-        print("Invalid response please enter Yes or No")
-        main()
+    print("This is a Password generator")
+    print("You have a choice of several lengths to make your password:")
+    print("1. 4 characters long")
+    print("2. 8 characters long")
+    print("3. 16 characters long")
+    print("4. 32 characters long")
+    print("5. 48 characters long")
+    print("6. 64 characters long")
+    print("7. 128 characters long")
+    choice = int(input("Choose a password length: "))
+    password_generator(choice)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
